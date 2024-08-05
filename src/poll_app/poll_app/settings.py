bf40baf65611d7c212b8b21bc9c41107be92b6f5
Alexander Gnusarev <alexander.gnussarev@gmail.com>
2024-08-05 22:08:02 +0600

    add toolbar

--- src/poll_app/poll_app/settings.py
+++ src/poll_app/poll_app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "poll_app.urls"
@@ -92,6 +94,10 @@ DATABASES = {
         "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Password validation
